df9841909d31316b17e163ad8752b879cdd7b3bb
Aidan <aidfrederick11@gmail.com>
2021-01-08 15:16:20 -0500

    Finished mern authentication backend

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -50,7 +50,7 @@ router.post("/login", async (req, res) => {
     if (!user) return res.status(400).json({ msg: "Account not found." });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+    if (!isMatch) return res.status(400).json({ msg: "Invalid password." });
 
     const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
     res.json({
@@ -59,7 +59,6 @@ router.post("/login", async (req, res) => {
             id: user._id,
             firstName: user.firstName,
             lastName: user.lastName,
-            email: user.email,
         },
     })
     
